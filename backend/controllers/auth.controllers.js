c66ac0bd6fb97a14667bcf703f8b08a2f2c040e5
rakesh <rakeshroshan878@gmail.com>
2024-08-14 00:21:43 +0530

    fixed the issue during user creation

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -6,13 +6,14 @@ export async function signup(req,res){
         if(!email || !password || !username){
             res.status(400).json({success : false, message :"All fields are required"})
         }
+        
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+    
         if(!emailRegex.test(email)){
-            res.staus(400).json({success : false, message : "Invalid Email"})
+            res.status(400).json({success : false, message : "Invalid Email"})
         }
-
-        if(password.length() < 6){
+        
+        if(password.length < 6){
             res.status(400).json({success : false, message : "Password length must be at least 6"})
         }
 
@@ -20,25 +21,24 @@ export async function signup(req,res){
         if(existingUserByEmail){
             res.status(400).json({success : false, message : "User with Email already exist"})
         }
-
+        
         const existingUserByUsername = await User.findOne({username : username})
         if(existingUserByUsername){
             res.status(400).json({success : false, message : "User with Username already exist"})
         }
-
+        
         const PROFILE_PICS = ["/avatar1.png","/avatar2.png","/avatar3.png"]
-
-        const image = PROFILE_PICS[Math.floor(Math.random()*PROFILE_PICS.length())]
+        const image = PROFILE_PICS[Math.floor(Math.random()*PROFILE_PICS.length)]
 
         const newUser = new User({
-            email : "email",
-            username : "username",
-            password : "password",
-            image : "image"
+            username,
+            email,
+            password,
+            image
+           
         })
 
         await newUser.save()
-
         res.status(201).json({
             success : true,
             user: {
@@ -47,7 +47,7 @@ export async function signup(req,res){
             }
 
         });
-        
+    
     }
     catch (error){
         res.status(500).json({ success: false , message : "Internal Server Error"})
--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import { ENV_VARS } from './config/envVars.js'
-import router from './router/auth.router.js'
+import authRoutes from './router/auth.router.js'
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // will allow to parse the req body.
-app.use("api/v1/auth", router)
+
+app.use("/api/v1/auth", authRoutes)
+
 const PORT = ENV_VARS.PORT
     
 app.listen(PORT,()=>{
-    console.log("server running at http://localhost:"+PORT)
+    console.log("server running at http://localhost:"+ PORT)
     connectDB();
 });
 
