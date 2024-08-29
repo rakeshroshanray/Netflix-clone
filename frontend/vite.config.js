f476214a0f84cec92e6a6eb48d33aa991a4cf10a
rakesh <rakeshroshan878@gmail.com>
2024-08-29 18:48:29 +0530

    fixed error insetting proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://netflix-clone-1-nqci.onrender.com', // Replace with your backend URL
+        target: 'http://netflix-clone-1-nqci.onrender.com/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false, // Disable SSL verification for self-signed certificates (if needed)
-        ws: true, // Enable WebSocket proxying (if using WebSockets)
+        rewrite: (path) => {
+          console.log(`Proxying request: ${path}`);
+          return path.replace(/^\/api/, '');
+        },
+        secure: false,
+        ws: true,
       }
     }
   }
