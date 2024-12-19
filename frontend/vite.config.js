39ab1b61add5eea643810c941ba8bd20f83aede5
rakeshroshanray <rakeshroshan878@gmail.com>
2024-12-19 16:26:14 +0530

    backend link added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target:  "http://localhost:5000/api",
+        target:  "https://netflix-clone-2-ad78.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => {
           console.log(`Proxying request: ${path}`);
